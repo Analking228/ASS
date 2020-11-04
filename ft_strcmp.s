d05ecc1f1a86fafb0f582ab579b4d116ee0f4732
Admin <admin@Mac-Admin.local>
2020-11-04 18:04:59 +0300

    geg]

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,17 +2,19 @@
 
 			section	.text
 _ft_strcmp:	
-			mov	rax, 0
-			mov rbx, 0
-			mov rcx, -1
+			mov		rax, 0
+			push	rbx
+			mov 	rbx, 0
+			mov 	rcx, -1
 	while:
-			inc	rcx
-			mov	al, BYTE[rdi + rcx]
-			mov bl, BYTE[rsi + rcx]
-			cmp al, 0
-			je	endwhile
-			cmp	al, bl
-			je	while
+			inc		rcx
+			mov		al, BYTE[rdi + rcx]
+			mov 	bl, BYTE[rsi + rcx]
+			cmp 	al, 0
+			je		endwhile
+			cmp		al, bl
+			je		while
 	endwhile:
-			sub rax, rbx
+			pop		rbx
+			sub 	rax, rbx
 			ret
